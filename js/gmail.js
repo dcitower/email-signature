145c88d5970560d3c7312651e1062f5a943244de
Jake Chapa <jakechapa@gmail.com>
2022-06-22 22:23:19 -0500

    use new auth api

--- js/gmail.js
+++ js/gmail.js
@@ -1,5 +1,9 @@
 // Enter an API key from the Google API Console:
 //   https://console.developers.google.com/apis/credentials
+
+// New project
+// https://console.cloud.google.com/apis/credentials?project=diverse-email-sig&supportedpurview=project
+
 const apiKey = 'YOUR_API_KEY';
 
 // Enter the API Discovery Docs that describes the APIs you want to
@@ -11,49 +15,69 @@ const discoveryDocs = ["https://people.googleapis.com/$discovery/rest?version=v1
 //   https://console.developers.google.com/apis/credentials?project=_
 // In your API Console project, add a JavaScript origin that corresponds
 //   to the domain where you will be running the script.
+
 const clientId = 'YOUR_CLIENT_ID';
 
 // Enter one or more authorization scopes. Refer to the documentation for
 // the API or https://developers.google.com/people/v1/how-tos/authorizing
 // for details.
-const scopes = 'profile';
+const scopes = [
+    "https://www.googleapis.com/auth/gmail.settings.basic",
+    "https://www.googleapis.com/auth/gmail.settings.sharing",
+    "https://www.googleapis.com/auth/gmail.readonly"
+]
 
 let authorizeButton;
 let signoutButton;
 let signatureButton;
 let myEmail;
 
+window.isAuthenticated = false;
+window.identity = {};
+window.token = '';
+
+let client;
+
 $(function() {
     authorizeButton = $('#authorize-button');
     signoutButton = $('#signout-button');
     signatureButton = $('#set-gmail-signature-button');
+
+    authorizeButton.click(onAuthClick);
+    signoutButton.click(onSignoutClick);
+    signatureButton.click(onSetSignatureClick);
+    
+    updateSigninStatus(false);
 })
 
 function handleClientLoad() {
-    // Load the API client and auth2 library
-    gapi.load('client:auth2', initClient);
-}
-
-function initClient() {
-    gapi.client.init({
-            apiKey: apiKey,
-            discoveryDocs: discoveryDocs,
-            clientId: clientId,
-            scope: scopes
-    }).then(function () {
-        // Listen for sign-in state changes.
-        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-        // Handle the initial sign-in state.
-        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-
-        authorizeButton.click(onAuthClick);
-        signoutButton.click(onSignoutClick);
-        signatureButton.click(onSetSignatureClick);
+    // Oauth Client Library Loaded
+    client = google.accounts.oauth2.initTokenClient({
+        client_id: clientId,
+        scope: scopes.join(' '),
+        callback: (response) => {
+            console.log('response', response)
+            window.at = response.access_token
+            window.gapi2 = gapi
+            if( response?.access_token ) {
+                gapi.client.setApiKey(response.access_token);
+                gapi.client.load('gmail', 'v1', handleApiLoadFinished);
+            }
+        }
     });
 }
 
+function handleApiLoad() {
+    // API Library Loaded
+    gapi.load('client');
+}
+
+function handleApiLoadFinished() {
+    updateSigninStatus(true);
+}
+
 function updateSigninStatus(isSignedIn) {
+    console.log('updateSigninStatus', isSignedIn)
     if (isSignedIn) {
         authorizeButton.hide()
         signoutButton.show()
@@ -67,15 +91,7 @@ function updateSigninStatus(isSignedIn) {
 }
 
 function onAuthClick(event) {
-    const scopes = [
-        "https://www.googleapis.com/auth/gmail.settings.basic",
-        "https://www.googleapis.com/auth/gmail.settings.sharing",
-        "https://www.googleapis.com/auth/gmail.readonly"
-    ]
-
-    gapi.auth2.getAuthInstance().signIn({
-        scope: scopes.join(' ')
-    });
+    client.requestAccessToken()
 }
 
 function onSignoutClick(event) {
@@ -83,23 +99,23 @@ function onSignoutClick(event) {
     $('#gmail-result').html('')
 }
 
-function onSetSignatureClick(event) 
+function onSetSignatureClick(event)
 {
     const signature = $('#code-textarea').val()
-    
+
     gapi.client.gmail.users.settings.sendAs.patch({
         'userId': 'me',
-        'sendAsEmail' : 'jake@dcitower.com',
+        'sendAsEmail' : myEmail,
         'signature' : signature
     }).then(function(response) {
         $('#gmail-result').html('√ Updated Gmail Signature')
     },
-    function(err) { 
+    function(err) {
         $('#gmail-result').html('x Error Updating Signature')
     });
 }
 
-function getProfile() 
+function getProfile()
 {
     gapi.client.gmail.users.getProfile({
         'userId': 'me'
